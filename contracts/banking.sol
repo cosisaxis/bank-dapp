249273694db49fe0782b072016d8bd10eb599c5f
cosisaxis <kishanelewis2@gmail.com>
2022-05-10 19:36:49 -0500

    tokens-created

--- contracts/banking.sol
+++ contracts/banking.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: Unlicensed 
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -23,20 +24,38 @@ contract Banking{
     function getWlSymbols() external view returns(bytes32[] memory){
         return wlSymbols;
     }
+
     function getWlTokenAddresses(bytes32 symbol) external view returns(address){
         return wlTokens[symbol];
     }
+
     receive() external payable{
         balances[msg.sender]['Eth'] += msg.value;
     }
+
     function removeEther(uint amount)external{
         require(balances[msg.sender]['Eth'] >= amount, 'You dont have enough funds to do this transaction');
 
         balances[msg.sender]['Eth'] -= amount;
-        payable(msg.sender).call{value: amount}("");
+       (bool success,) = payable(msg.sender).call{value: amount}("");
+       require(success, "no success");
+
     }
-    function tokenDeposit(uint256  amount, bytes32 symbol) external{
-        
+
+    function tokensDeposit(uint256  amount, bytes32 symbol) external{
+        balances[msg.sender][symbol] += amount;
+        IERC20(wlTokens[symbol]).transferFrom(msg.sender, address (this), amount);        
+    }
+
+    function removeTokens(uint256 amount, bytes32 symbol) external{
+        require(balances[msg.sender][symbol] >= amount, "You dont have enough funds to do this transact");
+
+        balances[msg.sender][symbol] -= amount;
+        IERC20(wlTokens[symbol]).transfer(msg.sender, amount);
+    }
+
+    function getTokenBalance(bytes32 symbol) external view returns(uint256){
+        return balances[msg.sender][symbol];
     }
 
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.0",
   },
   paths:{
     artifacts: "./client/src/artifacts",
